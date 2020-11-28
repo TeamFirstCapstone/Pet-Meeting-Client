d9e8bedb74457e1c687598975640ee3d69bbbb48
thswpvm <sonjeff@naver.com>
2020-11-28 15:11:20 +0900

    firebase attachment

--- src/App.js
+++ src/App.js
@@ -6,19 +6,6 @@ import Login from "./pages/Login";
 import Signup from "./pages/Signup";
 import MainPage from "./pages/MainPage";
 
-const firebase = require("firebase/analytics");
-
-var firebaseConfig = {
-  apiKey: "API_KEY",
-  authDomain: "PROJECT_ID.firebaseapp.com",
-  databaseURL: "https://PROJECT_ID.firebaseio.com",
-  projectId: "PROJECT_ID",
-  storageBucket: "PROJECT_ID.appspot.com",
-  messagingSenderId: "SENDER_ID",
-  appId: "APP_ID",
-  measurementId: "G-MEASUREMENT_ID",
-};
-
 function App() {
   return (
     <div className="App">
